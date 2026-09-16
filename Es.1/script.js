a64a44650177279686a18212cbcd1e52f3bb5b59
Matteo Regio <matteoregio08@gmail.com>
2026-09-16 09:44:36 +0200

    Completato il file index.html e lavorato su script.js

---
+++ Es.1/script.js
@@ -0,0 +1,11 @@
+const form = document.getElementById("form");
+const messaggio = document.getElementById("messaggio");
+const tabella = document.getElementById("tabella");
+
+form.addEventListener("submit", gestisciSubmit);
+
+const dati = [];
+
+function gestisciSubmit(event){
+    
+}
